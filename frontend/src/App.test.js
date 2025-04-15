13426b8543b974894cd3d249d43dda26582f56e0
RishabhBudhia <Budhiarishabh@gmail.com>
2025-04-15 10:29:16 -0700

    [Fix] Test Cases

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+import "@testing-library/jest-dom"; // Import jest-dom for toBeInTheDocument matcher
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders learn react link", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
+  const linkElement = screen.getByText(/Where/i);
   expect(linkElement).toBeInTheDocument();
 });
